73e32f19993a8e7bcd3aafc09d807d6068edefb4
Daniel <daniel-yo21@hotmail.com>
2026-02-05 15:44:57 +0100

    ex5

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,18 @@
+import express from 'express';
+
+const app = express();
+const port = 3000
+
+app.get('/', (req, res)=>{
+    res.send('Hello World!');
+})
+
+app.listen(port,() =>{
+    console.log(`Serveur lancé sur http://localhost:${port}`)
+})
+
+
+
 function greet(name: string): string{
     return `Hello ${name}!`
 }
