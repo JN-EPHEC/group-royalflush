d75094787c2f11d5abbb254a3dda228b9ca29ddb
danigmrs <he305254@students.ephec.be>
2026-02-16 19:21:57 +0100

    5.1 get,post,delete

--- public/script.js
+++ public/script.js
@@ -3,18 +3,45 @@ async function loadUsers(){
     const users = await reponse.json()
 
     const liste = document.getElementById('userList')
-
-
-    
-
-
+    liste.innerHTML = ""
     users.forEach(user => {
 
-
+        const btn = document.createElement("button")
         const li = document.createElement("li")
-        li.textContent = user.nom + " " + user.prenom
+        li.textContent = user.nom + " | " + user.prenom + " | "
+        btn.textContent = "X" 
+
+        btn.addEventListener("click", async()=>{
+            await fetch(`/api/users/${user.id}`,{
+                method: "DELETE"
+            })
+            loadUsers()
+        })
+        
+        li.appendChild(btn)
         liste.appendChild(li)
+
 
     })
 }
 loadUsers()
+
+const form = document.getElementById("userForm")
+
+form.addEventListener("submit", async(event)=>{
+    event.preventDefault()
+    let nom = document.getElementById("nom").value
+    let prenom = document.getElementById("prenom").value
+
+    await fetch("api/users",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({nom,prenom})
+    })
+    loadUsers()
+    form.reset() 
+})
+
+
