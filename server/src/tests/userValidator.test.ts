b02201e9f5ef2e508edc8c15a55b71b5ba7cfa92
danigmrs <he305254@students.ephec.be>
2026-03-10 22:34:03 +0100

    partie 7

---
+++ server/src/tests/userValidator.test.ts
@@ -0,0 +1,28 @@
+import { validateUserRegistration } from "../utils/userValidator"
+
+describe("User Registration Validator", ()=>{
+    test("utilisateur valide", ()=>{
+        expect(validateUserRegistration(25,"user","jreiozar@gmail.com")).toBe(true)
+    })
+    test("admin valide", ()=>{
+        expect(validateUserRegistration(25,"admin","jreiozar@gmail.com")).toBe(true)
+    })
+    test("âge invalide", ()=>{
+        expect(validateUserRegistration(130,"user","jreiozar@gmail.com")).toThrow("Âge invalide")
+    })
+    test("role invalide", ()=>{
+        expect(validateUserRegistration(25,"prof","jreiozar@gmail.com")).toThrow("Rôle invalide")
+    })
+    test("rejette si pas '@' et pas de '.' ", ()=>{
+        expect(validateUserRegistration(25,"user","jreiozargmail.com")).toBe(false)
+    })
+    test("rejette si quand '.'", ()=>{
+        expect(validateUserRegistration(25,"user","jreiozar@gmailcom")).toBe(false)
+    })
+    test("mineur refusé", ()=>{
+        expect(validateUserRegistration(15,"user","jreiozar@gmail.com")).toBe(false)
+    })
+    test("mineur STAGIAIRE accepeté", ()=>{
+        expect(validateUserRegistration(15,"stagiaire","jreiozar@gmail.com")).toBe(true)
+    })
+})
---
+++ server/src/utils/userValidator.ts
@@ -0,0 +1,31 @@
+import { error } from "node:console"
+
+export function validateUserRegistration(
+    age: number,
+    role: "admin" | "user" | "stagiaire",
+    email: string,
+): boolean {
+    // Age > 120 -> erreur
+    if (age > 120) {
+        throw new Error("Âge invalide")
+    }
+
+    // Verification rôle
+    if(!["admin","user","stagiaire"].includes(role)){
+        throw new Error("Rôle invalide")
+    }
+
+    //Email invalide
+    if(!email.includes("@") || !email.includes(".")){
+        return false
+    }
+    // âge inférieur à 18
+    if (age < 18){
+        if(role === "stagiaire"){
+            return true
+        }
+    }else {
+        return false
+    }
+    return true
+}
